0477a5b1a6f57eeef38b3835ea19edefebf1e416
Sreyas-R <sreyasrejil1@gmail.com>
2023-03-28 02:12:26 +0530

    Dashboard Added

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import Hero from "./components/Hero";
 import Heroine from "./components/Heroine";
 import Footer from "./components/Footer";
 import Registration from "./components/Registration";
+import PieChart from "./components/PieChart";
+import Dashboard from "./components/Dashboard";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        <Registration />
+        <Navbar />
       </div>
     </div>
+    <Dashboard />
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-        <Heroine />
-        <Footer />
-      </div>
+      <div className={`${styles.boxWidth}`}></div>
     </div>
   </div>
 );
